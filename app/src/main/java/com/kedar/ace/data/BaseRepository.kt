167aed1d33bdf48d98fdd199753691e014fb7431
Kedar Tendolkar <kedar.tendolkar@robosoftin.com>
2018-10-07 23:42:50 +0530

    - Removed call adapter from Retrofit

--- app/src/main/java/com/kedar/ace/data/BaseRepository.kt
+++ app/src/main/java/com/kedar/ace/data/BaseRepository.kt
@@ -4,7 +4,6 @@ import com.google.gson.Gson
 import com.google.gson.GsonBuilder
 import com.kedar.ace.BASE_URL
 import retrofit2.Retrofit
-import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory
 import retrofit2.converter.gson.GsonConverterFactory
 
 
@@ -22,7 +21,6 @@ abstract class BaseRepository {
 
         return Retrofit.Builder()
                 .baseUrl(BASE_URL)
-                .addCallAdapterFactory(RxJava2CallAdapterFactory.create())
                 .addConverterFactory(GsonConverterFactory.create(getGson()))
                 .build()
     }
