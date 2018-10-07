eeacb7848bc45d638ea13065adf37a955cb9c7ba
Kedar Tendolkar <kedar.tendolkar@robosoftin.com>
2018-10-07 23:21:21 +0530

    - Renamed MainActivity to ItemListActivity

--- app/src/main/java/com/kedar/ace/ui/ItemListActivity.kt
+++ app/src/main/java/com/kedar/ace/ui/ItemListActivity.kt
@@ -1,12 +1,13 @@
-package com.kedar.ace
+package com.kedar.ace.ui
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import com.kedar.ace.R
 
-class MainActivity : AppCompatActivity() {
+class ItemListActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_item_list)
     }
 }
