836d51cae2bf5f4ee04e52321c6670f3303a647d
Kedar Tendolkar <kedar.tendolkar@robosoftin.com>
2018-10-07 23:30:09 +0530

    - Added ItemModel for API - Added adapter for RecyclerView

---
+++ app/src/main/java/com/kedar/ace/model/ItemModel.kt
@@ -0,0 +1,19 @@
+package com.kedar.ace.model
+
+import com.google.gson.annotations.SerializedName
+
+
+data class DataModel(
+        @SerializedName("title")
+        val mTitle: String? = null,
+        @SerializedName("rows")
+        val mRowsEntity: List<RowsEntity>? = null
+)
+
+data class RowsEntity(
+        @SerializedName("title")
+        val mTitle: String,
+        @SerializedName("description")
+        val mDescription: String,
+        @SerializedName("imageHref")
+        val mImagehref: String)
---
+++ app/src/main/java/com/kedar/ace/ui/ItemListAdapter.kt
@@ -0,0 +1,51 @@
+package com.kedar.ace.ui
+
+import android.content.Context
+import android.support.v7.widget.AppCompatTextView
+import android.support.v7.widget.RecyclerView
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import com.bumptech.glide.Glide
+import com.kedar.ace.R
+import com.kedar.ace.model.RowsEntity
+
+
+class ItemListAdapter internal constructor(context: Context) : RecyclerView.Adapter<ItemListAdapter.ItemViewHolder>() {
+
+    private val mInflater: LayoutInflater = LayoutInflater.from(context)
+    private var mItems: List<RowsEntity>? = null
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ItemViewHolder {
+        val itemView = mInflater.inflate(R.layout.list_item, parent, false)
+        return ItemViewHolder(itemView)
+    }
+
+    override fun getItemCount(): Int {
+        return if (mItems != null)
+            mItems!!.size
+        else 0
+    }
+
+    override fun onBindViewHolder(holder: ItemViewHolder, position: Int) {
+        if (mItems != null) {
+            val current: RowsEntity = mItems!!.get(position)
+            holder.txtTitle.text = current.mTitle
+            holder.txtDescription.text = current.mDescription
+
+            Glide.with(holder.imgThumbnail).load(current.mImagehref).into(holder.imgThumbnail)
+        }
+    }
+
+    internal fun setItems(rowEntries: List<RowsEntity>?) {
+        mItems = rowEntries
+        notifyDataSetChanged()
+    }
+
+    inner class ItemViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        val txtTitle: AppCompatTextView = itemView.findViewById(R.id.txt_title)
+        val txtDescription: AppCompatTextView = itemView.findViewById(R.id.txt_description)
+        val imgThumbnail: ImageView = itemView.findViewById(R.id.img_thumbnail)
+    }
+}
